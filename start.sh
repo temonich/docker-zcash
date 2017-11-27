298f755bbccecafe436a967a3a1a67e981ccf3a8
Patrick Dreismann <patdreis@mai.uni-paderborn.de>
2017-11-27 15:19:35 +0100

    Change zcash.conf init to suggested commands in zcash wiki

--- start.sh
+++ start.sh
@@ -3,10 +3,10 @@
 ZCASH_CONF="$HOME/.zcash/zcash.conf"
 
 if ! [ -f "$ZCASH_CONF" ]; then
-    mkdir -p $HOME/.zcash/
-    echo "rpcuser=zcash" > ${ZCASH_CONF}
-    echo "rpcpassword=`pwgen 20 1`" >> ${ZCASH_CONF}
-    echo "addnode=mainnet.z.cash" >> ${ZCASH_CONF}
+	mkdir -p $HOME/.zcash/
+	echo "addnode=mainnet.z.cash" > ${ZCASH_CONF}
+	echo "rpcuser=username" >> ${ZCASH_CONF}
+	echo "rpcpassword=`head -c 32 /dev/urandom | base64`" >> ${ZCASH_CONF}
 fi
 
 zcash-fetch-params
